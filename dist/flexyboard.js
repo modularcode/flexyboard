fa22d16284ba99d2f156feb8e7aab7d4e78ada45
Gevorg Harutyunyan <gevharut@gmail.com>
2017-03-10 03:07:28 +0400

    Build scripts update

--- dist/flexyboard.js
+++ dist/flexyboard.js
@@ -1,12 +1,65 @@
+// On Init
+
+  // Stick header to left
+  // Stick sidebar to bottom
+  // Stick sidebar secondary to top
+  // Stick sidebar secondary to bottom
+
+
+
+
+
 // Toggle sidebar fixed
+
+  // Default mode
+
+    // If the header isn't sticked to right, stick secondary sidebar to top
+    // If the footer isn't sticked to top, stick secondary sidebar to right
+
+  // RTL mode
+
+    // If the header isn't sticked to right, stick sidebar to top
+    // If the footer isn't sticked to top, stick sidebar to right
 
 
 // Toggle sidebar-secondary fixed
 
+  // Default mode
 
+    // If the header isn't sticked to right, stick secondary sidebar to top
+    // If the footer isn't sticked to top, stick secondary sidebar to right
+
+  // RTL mode
+
+    // If header isn't sticked to left, stick secondary sidebar to top
+    // If footer isn't sticked to left, stick secondary sidebar to botton
 
 // Toggle header fixed
 
+  // Default mode
 
+    // If the sidebar isn't sticked to top, stick header to left
+    // If sidebar secondary isn't sticked to top, stick header to right
+
+  // RTL mode
+
+    // If header isn't sticked to left, stick sidebar to top
+    // If footer isn't sticked to left, stick sidebar to botton
 
 // Toggle footer fixed
+
+  // Default mode
+
+    // If the sidebar isn't sticked to bottom, stick footer to left
+    // If sidebar secondary isn't sticked to bottom, stick footer to right
+
+  // RTL mode
+
+    // If the sidebar isn't sticked to bottom, stick footer to right
+    // If sidebar secondary isn't sticked to bottom, stick footer to left
+
+
+// Toggle RTL mode
+
+  // toggle rtl class
+  // inverse vertical stickings
